6506029c815dcabe029a0c26f902022ea1375a5a
unknown <tayyx2000@163.com>
2020-11-30 14:55:11 +0800

    202011302

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,12 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MAXN = 100050;
-const int MAXM = 200050;
 struct edge{
 	int v,w,nx;
-	
-}E[MAXM];
-int tot, h[MAXN];
+};
+edge *E;
+int tot, *h, *pre;
 void add_edge(int x, int y,int c){
 	E[++tot] = (edge){y, c, h[x]};
 	h[x] = tot;
@@ -19,12 +17,16 @@ struct node{
 	}
 };
 priority_queue<node> Q;
-int dis[MAXN];
-bool vis[MAXN];
+int *dis;
+bool *vis;
+int n, m, s;
 void dij(int s){
-	memset(dis, 0x7f, sizeof dis);
-	memset(vis, 0, sizeof vis);
+	for(int i=1;i<=n;i++){
+		dis[i] = 0x7fffffff;
+		vis[i] = false;
+	}
 	dis[s] = 0;
+	pre[s] = 0;
 	Q.push((node){0, s});
 	while(!Q.empty()){
 		node cur = Q.top();
@@ -35,6 +37,7 @@ void dij(int s){
 			int o = E[i].v;
 			if(dis[o] > dis[cur.p] + E[i].w){
 				dis[o] = dis[cur.p] + E[i].w;
+				pre[o] = cur.p;
 				if(!vis[o]){
 					Q.push((node){dis[o], o});
 				}
@@ -42,9 +45,15 @@ void dij(int s){
 		}
 	}
 }
-int n, m, s;
+
 int main(){
 	scanf("%d%d%d", &n, &m ,&s);
+	h = new int[n+1];
+	for(int i=1;i<=n;i++)h[i]=0;
+	E = new edge[m+1];
+	dis = new int[n+1];
+	vis = new bool[n+1];
+	pre = new int[n+1];
 	for(int i=1;i<=m;i++){
 		int a,b,c;
 		scanf("%d%d%d",&a,&b,&c);
@@ -54,5 +63,26 @@ int main(){
 	for(int i=1;i<=n;i++){
 		printf("%d ", dis[i]);
 	}
+	puts("");
+	for(int i=1;i<=n;i++){
+		printf("第%d点的最短路径\n",i);
+		int cur = i;
+		while(cur != 0){
+			printf("%d ",cur);
+			cur = pre[cur];
+		}
+		puts("");
+	}
 	return 0;
 }
+/*
+test case
+4 6 1
+1 2 2
+2 3 2
+2 4 1
+1 3 5
+3 4 3
+1 4 4
+
+*/
